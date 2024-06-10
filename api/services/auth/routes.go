aeb2e12834c9c0ea885ea3c0d8c3212fa7a172db
William Kennedy <bill@ardanlabs.com>
2024-06-10 12:41:53 -0400

    commit 8

--- api/services/auth/routes.go
+++ api/services/auth/routes.go
@@ -7,7 +7,7 @@ type token struct {
 }
 
 //lint:ignore U1000 "called by encore"
-//encore:api method=GET path=/token/:kid
+//encore:api public method=GET path=/token/:kid
 func Token(ctx context.Context, kid string) (token, error) {
 	t := token{
 		Value: kid + "-1",
