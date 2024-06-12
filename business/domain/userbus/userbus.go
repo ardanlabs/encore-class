6fcc28b0231641623f7ee7a52614807c7c1c43b2
William Kennedy <bill@ardanlabs.com>
2024-06-12 12:51:44 -0400

    commit 32

--- business/domain/userbus/userbus.go
+++ business/domain/userbus/userbus.go
@@ -1,2 +1,31 @@
 // Package userbus provides business access to user domain.
 package userbus
+
+import (
+	"context"
+	"errors"
+	"net/mail"
+
+	"github.com/ardanlabs/encore/business/sdk/order"
+	"github.com/ardanlabs/encore/business/sdk/page"
+	"github.com/google/uuid"
+)
+
+// Set of error variables for CRUD operations.
+var (
+	ErrNotFound              = errors.New("user not found")
+	ErrUniqueEmail           = errors.New("email is not unique")
+	ErrAuthenticationFailure = errors.New("authentication failed")
+)
+
+// Storer interface declares the behavior this package needs to perists and
+// retrieve data.
+type Storer interface {
+	Create(ctx context.Context, usr User) error
+	Update(ctx context.Context, usr User) error
+	Delete(ctx context.Context, usr User) error
+	Query(ctx context.Context, filter QueryFilter, orderBy order.By, page page.Page) ([]User, error)
+	Count(ctx context.Context, filter QueryFilter) (int, error)
+	QueryByID(ctx context.Context, userID uuid.UUID) (User, error)
+	QueryByEmail(ctx context.Context, email mail.Address) (User, error)
+}
